eb77df76ce6ea31467d34a1bada0b03e0132d5b4
tyeborg <supersadtyler@gmail.com>
2023-04-12 23:30:21 +0100

    Adding additional functionality to detector tool within app.js

--- flaskapp/app/static/app.js
+++ flaskapp/app/static/app.js
@@ -1,8 +1,8 @@
 // Initialize variables for form switching by button clicks.
-const imageForm = document.querySelector("form.img-upload")
-const videoForm = document.querySelector("form.video-upload")
-const imageBtn = document.querySelector("label.image-toggle")
-const videoBtn = document.querySelector("label.video-toggle")
+const imageForm = document.querySelector("form.imgUpload")
+const videoForm = document.querySelector("form.videoUpload")
+const imageBtn = document.querySelector("label.imageToggle")
+const videoBtn = document.querySelector("label.videoToggle")
 
 // Switch from imageForm to videoForm upon button click.
 videoBtn.onclick = (() => {
@@ -14,69 +14,77 @@ imageBtn.onclick = (() => {
     imageForm.style.marginLeft = "0%";
 });
 
-// ------------------------------------------------------------ //
-
+// Initialize variables to display the image or video submitted by the user.
 const inputImg = document.querySelector("#imgUploadBtn");
 const inputVid = document.querySelector("#videoUploadBtn");
 const imgAreaDisplay = document.querySelector("#displayImg");
 const videoAreaDisplay = document.querySelector("#displayVideo");
 
+// Determine if the video submitted by the user is appropriate for processing.
 inputVid.addEventListener('change', function () {
+    // Obtain the file submitted by the user.
     const videoFile = this.files[0];
 
+    // Initialize variables for the validation section and video process button.
     const videoErrorArea = document.querySelector("#videoErrorArea");
     const videoUploadedArea = document.querySelector("#videoUploadedArea");
     const videoProcessBtn = document.querySelector("#videoProcessBtn");
 
-    // Initialize a list of all the valid file extensions
+    // Initialize a list of all the valid file extensions.
     let validExtensions = ['video/mp4', 'video/mov'];
 
     validateFile(videoFile, validExtensions, videoUploadedArea, videoErrorArea, videoProcessBtn);
 
+    // Determine if the input video submitted by the user is a 
+    // video of an mp4 or mov type.
     if(validExtensions.includes(videoFile.type)) {
-        // Display the image in the 'upload image' area.
+        // Display the video in the 'Upload Image' area.
         displayVideo(videoFile);
     }
 });
 
+// Determine if the image submitted by the user is appropriate for processing.
 inputImg.addEventListener('change', function () {
+    // Obtain the file submitted by the user.
     const image = this.files[0];
 
+    // Initialize variables for the validation section and image process button.
     const imgErrorArea = document.querySelector("#imgErrorArea");
     const imgUploadedArea = document.querySelector("#imgUploadedArea");
     const imgProcessBtn = document.querySelector("#imgProcessBtn");
 
-    // Initialize a list of all the valid file extensions
+    // Initialize a list of all the valid image extensions.
     let validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
 
     validateFile(image, validExtensions, imgUploadedArea, imgErrorArea, imgProcessBtn);
 
+    // Determine if the file sent by the user is of a validated image file type.
     if(validExtensions.includes(image.type)) {
-        // Display the image in the 'upload image' area.
+        // Display the image in the 'Upload Image' area.
         displayImg(image);
     }
 });
 
 // Construct a function that validates a file with a corresponding file extension.
 function validateFile(file, validExtensions, uploadedArea, errorArea, processBtn) {
-    // Ensure the right file type was uploaded
-    // Initialize a variable defining the file type of the uploaded file
+    // Ensure the right file type was uploaded.
+    // Initialize a variable defining the file type of the uploaded file.
     let fileType = file.type;
     let altFileType = fileType.split("/")[1];
 
     if(validExtensions.includes(fileType)) {
-        // Setting selected file name
+        // Setting selected file name.
         let fileName = file.name;
         // Initialize a variable for the file size.
         let fileSize = convertBytes(file.size);
         
         // Alter the filename depending if it is too long or not.
-        if(fileName.length >= 8) {
+        if(fileName.length >= 6) {
             let splitName = fileName.split('.');
-            fileName = splitName[0].substring(0, 8) + "... ." + splitName[1];
+            fileName = splitName[0].substring(0, 6) + "... ." + splitName[1];
         }
 
-        // Clear the progress area, in case the user uploads additional files
+        // Clear the progress area, in case the user uploads additional files.
         errorArea.innerHTML = "";
 
         let uploadedHTML = `<li class="row">
@@ -122,25 +130,24 @@ function displayVideo(videoFile) {
     const reader = new FileReader();
     reader.onload = () => {
         // Remove the previous item in the upload queue.
-        const allImg = videoAreaDisplay.querySelectorAll('video');
-        allImg.forEach(item => item.remove());
+        const allVid = videoAreaDisplay.querySelectorAll('video');
+        allVid.forEach(item => item.remove());
         
         // Read the newly uploaded video.
         const vidUrl = reader.result;
         const vid = document.createElement('video');
 
         vid.src = vidUrl
-        // Add the image to the image area when user has uploaded it.
+        // Add the video to the video area when user has uploaded it.
         videoAreaDisplay.appendChild(vid);
         vid.controls = true;
         vid.load();
-        //vid.play();
         videoAreaDisplay.classList.add('active');
     }
     reader.readAsDataURL(videoFile)
 }
 
-function displayImg(image) {
+function displayImg(imageFile) {
     // Make the newly uploaded image display on the upload image area.
     const reader = new FileReader();
     reader.onload = () => {
@@ -156,13 +163,14 @@ function displayImg(image) {
         imgAreaDisplay.appendChild(img);
         imgAreaDisplay.classList.add('active');
     }
-    reader.readAsDataURL(image)
+    reader.readAsDataURL(imageFile)
 }
 
 // Create a function that determines the size of a file.
 function convertBytes(num) {
     // Initialize variables.
     let diff = num < 0;
+    // List all of the potential file sizes.
     let units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
     if (diff) {
@@ -172,6 +180,7 @@ function convertBytes(num) {
         return (diff ? '-' : '') + num + ' B';
     }
 
+    // Exact the file size.
     let exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
     num = Number((num / Math.pow(1000, exponent)).toFixed(2));
     let unit = units[exponent];
@@ -179,26 +188,65 @@ function convertBytes(num) {
     return (diff ? '-' : '') + num  + ' ' + unit;
 }
 
-function handleFormSubmit() {
-    //const processBtn = document.querySelector(".processBtn");
+// Construct a function that submits a form form once process button is clicked.
+function formSubmit(processBtn, formType) {
+    processBtn.addEventListener("click", function () {
+        formType.submit();
+    });
+}
+
+// Enable onclick functionality to Image Process button.
+function handleImgFormSubmit() {
     const imgProcessBtn = document.querySelector("#imgProcessBtn");
     const imgForm = document.querySelector("#imgForm");
-    //processBtn.classList.add("btnLoading");
-    //processBtn.classList.remove("btnLoading");
 
-    imgProcessBtn.addEventListener("click", function () {
-        imgForm.submit();
+    // Submit Image form once Image process button is clicked.
+    formSubmit(imgProcessBtn, imgForm);
+}
 
-        window.addEventListener("load", () => {
-            imgProcessBtn.classList.add("btnLoading");
+// Enable onclick functionality to Video Process button.
+function handleVideoFormSubmit() {
+    const videoProcessBtn = document.querySelector("#videoProcessBtn");
+    const videoForm = document.querySelector("#videoForm");
+
+    // Submit Video form once Video process button is clicked.
+    formSubmit(videoProcessBtn, videoForm);
+}
+
+// Construct a method that resets everything to its default
+// functionality by clicking the 'RESET' button.
+const resetBtn = document.querySelector("#resetBtn");
+resetBtn.onclick = function() {
+    const selectors = [
+        "#displayImg > img:last-of-type",
+        "#displayVideo > video:last-of-type",
+        ".faceCaptureImg > img:last-of-type",
+        ".faceCaptureImg > video:last-of-type",
+        ".validationSection > li:last-of-type",
+        ".classificationSection > li:last-of-type"
+    ];
+
+    // Iterate throughout the list to set everything to default.
+    selectors.forEach(selector => {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(element => {
+            element.remove();
         });
     });
+};
 
-    //window.addEventListener("load", () => {
-        //processBtn.classList.add("btnLoading");
-
-        //processBtn.addEventListener("transitionend", () => {
-            //processBtn.classList.remove("btnLoading");
-        //})
-    //})  
+// Create a function that add spinner effect to button once clicked.
+function enableButtonSpinner(processBtn) {
+    processBtn.addEventListener("click", function () {
+        // Add spinner functionality once button is clicked.
+        processBtn.classList.add("btnLoading");
+    });
 }
+
+// Initialize the process buttons for the spinner effect.
+const imgProcessBtn = document.querySelector("#imgProcessBtn");
+const videoProcessBtn = document.querySelector("#videoProcessBtn");
+
+// Enable spinning effect for Image and Video Process buttons once clicked.
+enableButtonSpinner(imgProcessBtn);
+enableButtonSpinner(videoProcessBtn);
