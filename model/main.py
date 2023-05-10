67e95a8afc69d7968900958d5f8790cb39f76ca3
tyeborg <supersadtyler@gmail.com>
2023-05-10 23:38:11 +0100

    Commiting the architectures used for the study

--- model/main.py
+++ model/main.py
@@ -4,14 +4,51 @@ import glob
 #import cv2 as cv
 import numpy as np
 import pandas as pd
+import plotly.graph_objs as go
+from plotly.offline import iplot
 from model import DeepfakeDetectiveModel
+from preprocess import Preprocess, FaceForensics
 from sklearn.model_selection import train_test_split
-from preprocess import Preprocess, FaceForensics, RealAndFakeFaces
+
+def plot_sets(Train_set, Val_set, Test_set):
+    y = dict()
+
+    y[0] = []
+    y[1] = []
+
+    for set_name in (np.array(Train_set['label']), np.array(Val_set['label']), np.array(Test_set['label'])):
+        y[0].append(np.sum(set_name == 'REAL'))
+        y[1].append(np.sum(set_name == 'FAKE'))
+
+    trace0 = go.Bar(
+        x=['Train Set', 'Validation Set', 'Test Set'],
+        y=y[0],
+        name='REAL',
+        marker=dict(color='#2696C2'),
+        opacity=0.7
+    )
+    trace1 = go.Bar(
+        x=['Train Set', 'Validation Set', 'Test Set'],
+        y=y[1],
+        name='FAKE',
+        marker=dict(color='#A5104B'),
+        opacity=0.7
+    )
+
+    data = [trace0, trace1]
+    layout = go.Layout(
+        title='Count of Images in Each Set',
+        xaxis={'title': 'Set'},
+        yaxis={'title': 'Count'}
+    )
+
+    fig = go.Figure(data, layout)
+    iplot(fig)
 
 def get_data(metadata_path):
     return pd.read_csv(metadata_path)
 
-def get_metadatas(face_forensics_base, real_and_fake_base):
+def get_metadatas(face_forensics_base):
     # Create FaceForensics object.
     face_forensics = FaceForensics(face_forensics_base)
     # Extract frames and allocate them to their respective directories.
@@ -20,16 +57,9 @@ def get_metadatas(face_forensics_base, real_and_fake_base):
     # Convert dataframe into a csv file.
     face_forensics_df.to_csv(face_forensics_base + 'metadata.csv', index=False)
 
-    # Create RealAndFakeFaces object.
-    real_and_fake = RealAndFakeFaces(real_and_fake_base)
-    real_and_fake_df = real_and_fake.meta
-    # Convert dataframe into a csv file.
-    real_and_fake_df.to_csv(real_and_fake_base + 'metadata.csv', index=False)
-
 def main():
     # Declare the basepaths for our datasets.
-    #face_forensics_base = './datasets/face_forensics/'
-    #real_and_fake_base = './datasets/real_and_fake_face_detection/'
+    face_forensics_base = './datasets/face_forensics/'
     
     # Use this code snippet to create metadata from the datasets.
     #get_metadatas(face_forensics_base, real_and_fake_base)
@@ -39,27 +69,40 @@ def main():
 
     # Read the Face Forensics metadata.
     #face_forensics_df = get_data(face_forensics_base + 'metadata.csv')
-    # Read the Real and Fake Faces Detection metadata.
-    #real_and_fake_df = get_data(real_and_fake_base + 'metadata.csv')
-
-    # Receive a merged and sampled meta.
-    #sampled_meta = preprocess.get_sampled_meta(face_forensics_df, real_and_fake_df)
 
     # Export result in csv.
     #sampled_meta.to_csv('./datasets/final_metadata.csv', index=False)
-    sampled_meta = pd.read_csv('./datasets/face_forensics/metadata.csv')
+    sampled_meta = pd.read_csv(face_forensics_base + 'metadata.csv')
+
+    real_df = sampled_meta[sampled_meta["label"] == "REAL"]
+    fake_df = sampled_meta[sampled_meta["label"] == "FAKE"]
+    sample_size = 8500
+
+    real_df = real_df.sample(sample_size, random_state=42)
+    fake_df = fake_df.sample(sample_size, random_state=42)
+
+    sampled_meta = pd.concat([real_df, fake_df])
 
     # Obtain training, testing, and validation sets from 'data_fused_meta'.
-    train_set, test_set = train_test_split(sampled_meta,test_size=0.15,random_state=42,stratify=sampled_meta['label'])
-    train_set, val_set = train_test_split(train_set,test_size=0.15,random_state=42,stratify=train_set['label'])
+    train_set, test_set = train_test_split(sampled_meta,test_size=0.30,random_state=42,stratify=sampled_meta['label'])
+    test_set, val_set = train_test_split(test_set,test_size=0.5,random_state=42,stratify=test_set['label'])
+
+    print(f'Train set: {train_set.shape}')
+    print(f'Validation set: {val_set.shape}')
+    print(f'Test set: {test_set.shape}')
+
+    plot_sets(train_set, val_set, test_set)
 
     # Segmentize sets between the images (x) and labels (y).
-    x_train, y_train = preprocess.get_segment_sets(train_set)
-    x_val, y_val = preprocess.get_segment_sets(val_set)
-    x_test, y_test = preprocess.get_segment_sets(test_set)
+    x_train, y_train = preprocess.get_segment_sets(train_set, name="Train Set")
+    x_val, y_val = preprocess.get_segment_sets(val_set, name="Validation Set")
+    x_test, y_test = preprocess.get_segment_sets(test_set, name="Test Set")
 
-    effnet = DeepfakeDetectiveModel(x_train, y_train, x_val, y_val)
+    effnet = DeepfakeDetectiveModel(x_train, y_train, x_val, y_val, x_test, y_test)
     effnet.train()
+
+    #custom_cnn = CustomCNNModel(x_train, y_train, x_val, y_val, x_test, y_test)
+    #custom_cnn.train()
 
 if __name__ =='__main__':
     main()
--- model/model.py
+++ model/model.py
@@ -2,120 +2,197 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import tensorflow as tf
+from functools import partial
 import matplotlib.pyplot as plt
 from tensorflow.data import Dataset
 from tensorflow.keras import Sequential
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import confusion_matrix
 from tensorflow.keras.backend import clear_session
+from tensorflow.keras.optimizers.legacy import Adam
 from tensorflow.keras.applications import efficientnet, EfficientNetB0
-from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, GlobalAveragePooling2D
 from tensorflow.keras.layers import Input, RandomRotation, RandomTranslation, RandomFlip, RandomContrast
 from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
+from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D, Conv2D, MaxPooling2D, Flatten
 
 # Setting image dimension.
 IMG_HEIGHT = 224
 IMG_WIDTH =  224
 
-class DeepfakeDetectiveModel():
-    def __init__(self, x_train, y_train, x_val, y_val):
-        self.train_set_raw = self.create_raw_sets(x_train, y_train)
-        self.val_set_raw = self.create_raw_sets(x_val, y_val)
+class MyModel():
+    def __init__(self, x_train, y_train, x_val, y_val, x_test, y_test):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_val = x_val
+        self.y_val = y_val
+        self.x_test = x_test
+        self.y_test = y_test
 
-    def create_raw_sets(self, x_set, y_set):
-        return(Dataset.from_tensor_slices((x_set,y_set)))
+    def display_confusion_matrix(self, cm):
+        sns.set(font_scale=1.2)
+        sns.set_style("white")
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", cbar=True)
+        plt.xlabel("Predicted Labels", fontsize=14)
+        plt.ylabel("True Labels", fontsize=14)
+        plt.title("Model Applied Towards Test Set", fontsize=16)
+        plt.show()
+
+    def plot_performance(self, model_fit, loss_ylim_min, loss_ylim_max):
+        acc = model_fit.history['accuracy']
+        val_acc = model_fit.history['val_accuracy']
+        loss_ = model_fit.history['loss']
+        val_loss_ = model_fit.history['val_loss']
+
+        # Get the number of epochs from the history object
+        num_epochs = len(model_fit.history['loss'])
+
+        plt.figure(figsize=(8, 8))
+        plt.subplot(2, 1, 1)
+        plt.plot(acc, label='Training Accuracy')
+        plt.plot(val_acc, label='Validation Accuracy')
+        plt.xlim([1, num_epochs])
+        plt.ylim([0.5, 1])
+
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
+
+        plt.subplot(2, 1, 2)
+        plt.plot(loss_, label='Training Loss')
+        plt.plot(val_loss_, label='Validation Loss')
+        plt.xlim([1, num_epochs])
+        plt.ylim([loss_ylim_min, loss_ylim_max])
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.xlabel('Epochs')
+        plt.savefig('epoch-performance.png', bbox_inches='tight')
+
+class DeepfakeDetectiveModel(MyModel):
+    def __init__(self, x_train, y_train, x_val, y_val, x_test, y_test):
+        super().__init__(x_train, y_train, x_val, y_val, x_test, y_test)
 
     def build_model(self):
-        # Data augmentation to reduce overtraining.
-        data_augmentation = Sequential(
-            [
-                RandomRotation(factor=0.15),
-                RandomTranslation(height_factor=0.1, width_factor=0.1),
-                RandomFlip(),
-                RandomContrast(factor=0.1),
-            ],
-            name="data_augmentation",
-        )
-
         # Initialize the base model with pre-trained ImageNet weights, and fine-tune it on the dataset.
         # Load the Pretrained Base Model (using the B0 version).
-        inputs = Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3))
-        x = data_augmentation(inputs)
-        base_model = EfficientNetB0(include_top=False, input_tensor=x, weights='imagenet')
+        tf.random.set_seed(42)
+        base_model = EfficientNetB0(weights="imagenet", include_top=False)
 
-        # Unfreeze the pretrained weights.
-        base_model.trainable = True
+        # Unfreeze the last 75 pre-trained layers.
+        for layer in base_model.layers[-75:]:
+            layer.trainable = True
 
-        # Unfreeze the top 20 layers while leaving BatchNorm layers frozen.
-        #for layer in base_model.layers[-20:]:
-            #if not isinstance(layer, layers.BatchNormalization):
-                #layer.trainable = True
-
-        # Build the model.
+        # Add new layers for classification
         x = base_model.output
-        x = GlobalAveragePooling2D(name="avg_pool")(x)
-        outputs = Dense(1, activation="sigmoid", name="pred")(x)
+        x = GlobalAveragePooling2D()(x)
+        outputs = Dense(1, activation='sigmoid')(x)
 
         # Compile.
-        model = Model(inputs, outputs, name="EfficientNet")
-        opt = Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+        model = Model(inputs=base_model.input, outputs=outputs)
+        opt = Adam(learning_rate=0.00001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
         model.compile(optimizer=opt, loss="binary_crossentropy", metrics=["accuracy"])
 
         return model
         
-    def input_preprocess(self):
-        # Extra code – resets layer name counter.
-        clear_session()
-
-        batch_size = 32
-        preprocess = efficientnet.preprocess_input
-        train_set = self.train_set_raw.map(lambda x, y: (preprocess(tf.cast(x, tf.float32)), y))
-        train_set = train_set.shuffle(1000, seed=42).batch(batch_size).prefetch(1)
-        val_set = self.val_set_raw.map(lambda x, y: (preprocess(tf.cast(x, tf.float32)), y)).batch(batch_size)
-
-        return(train_set, val_set)
-        
     def train(self):
-        # Obtain training and validation set.
-        train_set, val_set = self.input_preprocess()
         # Receive the compiled model.
         model = self.build_model()
-        
-        earlystopping = EarlyStopping(monitor='val_accuracy', patience = 50,verbose = 1,mode = 'max')
-        checkpointer = ModelCheckpoint(filepath="effnet.hdf5", verbose=1, save_best_only=True)
-        reduce_lr = ReduceLROnPlateau(monitor='val_accuracy',patience=2,verbose=1,factor=0.5,min_delt=0.001,min_lr=0.00001)
 
+        # Define early stopping to prevent overfitting
+        early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='auto', restore_best_weights=True)
+        
         # Fit the model.
         history = model.fit(
-            train_set, 
-            epochs=2,
-            validation_data=val_set, 
-            callbacks = [checkpointer, reduce_lr, earlystopping],
+            self.x_train, 
+            self.y_train,
+            validation_data=(self.x_val, self.y_val), 
+            epochs=50,
+            batch_size=32,
+            callbacks=[early_stopping],
             verbose=1
         )
+
+        # Save the model.
+        #model.save("effnet.h5")
+        tf.saved_model.save(model, 'effnet')
+
+        # Evaluate the model on the test data.
+        test_loss, test_accuracy = model.evaluate(self.x_test, self.y_test)
+
+        # Print the test loss and accuracy.
+        print('Test loss:', test_loss)
+        print('Test accuracy:', test_accuracy)
+
         # Plot and save the model performance.
-        self.plot_performance(history)
+        super().plot_performance(history, loss_ylim_min=0, loss_ylim_max=0.8)
 
-    def plot_performance(self, history):
-        acc = history.history['accuracy']
-        val_acc = history.history['val_accuracy']
-        loss = history.history['loss']
-        val_loss = history.history['val_loss']
+        y_pred = model.predict(self.x_test)
+        # Convert to binary using threshold of 0.5.
+        y_pred_binary = (y_pred > 0.5).astype(int)
 
-        epochs = range(1, len(acc) + 1)
+        cm = confusion_matrix(self.y_test, y_pred_binary)
+        super().display_confusion_matrix(cm)
 
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
-        sns.set_style("white")
-        plt.suptitle('Train History', size = 15)
+class CustomCNNModel(MyModel):
+    def __init__(self, x_train, y_train, x_val, y_val, x_test, y_test):
+        super().__init__(x_train, y_train, x_val, y_val, x_test, y_test)
 
-        ax1.plot(epochs, acc, "bo", label = "Training Accuracy")
-        ax1.plot(epochs, val_acc, "b", label = "Validation Accuracy")
-        ax1.set_title("Training & Validation Accuracy")
-        ax1.legend()
-        ax1.savefig('acc.png', bbox_inches='tight')
+    def build_model(self):
+        tf.random.set_seed(42)
 
-        ax2.plot(epochs, loss, "bo", label = "Training Loss", color = 'red')
-        ax2.plot(epochs, val_loss, "b", label = "Validation Loss", color = 'red')
-        ax2.set_title("Training & Validation Loss")
-        ax2.legend()
-        ax2.savefig('loss.png', bbox_inches='tight')
+        # Define the input shape of the image
+        input_shape = (224, 224, 3)
+
+        # Define the Sequential model
+        model = tf.keras.Sequential([
+            Conv2D(32, (3, 3), input_shape=input_shape, activation='relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(32, (3, 3), activation='relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(64, (3, 3), activation='relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Flatten(),
+            Dense(64, activation='relu'),
+            Dropout(0.5),
+            Dense(1, activation='sigmoid')
+        ])
+
+        # Compile.
+        opt = Adam()
+        model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+        return model
+
+    def train(self):
+        # Receive the compiled model.
+        model = self.build_model()
+
+        # Define early stopping to prevent overfitting
+        early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='auto', restore_best_weights=True)
+        
+        # Fit the model.
+        history = model.fit(
+            self.x_train, 
+            self.y_train,
+            validation_data=(self.x_val, self.y_val), 
+            epochs=50,
+            batch_size=32,
+            callbacks=[early_stopping],
+            verbose=1
+        )
+
+        # Evaluate the model on the test data.
+        test_loss, test_accuracy = model.evaluate(self.x_test, self.y_test)
+
+        # Print the test loss and accuracy.
+        print('Test loss:', test_loss)
+        print('Test accuracy:', test_accuracy)
+
+        # Plot and save the model performance.
+        super().plot_performance(history, loss_ylim_min=0, loss_ylim_max=0.8)
+
+        y_pred = model.predict(self.x_test)
+        # Convert to binary using threshold of 0.5.
+        y_pred_binary = (y_pred > 0.5).astype(int)
+
+        cm = confusion_matrix(self.y_test, y_pred_binary)
+        super().display_confusion_matrix(cm)
